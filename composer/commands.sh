e6c5c5abf2ed5b45a3356b184d829430ceffa2ec
Andrea Pescetti <andrea@nuvole.org>
2016-09-25 01:38:14 +0200

    Add the ds module, version 8.x-2.5.

--- composer/commands.sh
+++ composer/commands.sh
@@ -3,3 +3,7 @@
 # Create a new Drupal project.
 composer create-project drupal-composer/drupal-project:8.x-dev my-project --stability dev --no-interaction
 
+# Add the ds module, version 8.x-2.5.
+composer require drupal/ds 8.2.5
+
+
