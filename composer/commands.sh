6745780f29fde9e6b33b7d6545a87f314292555b
Andrea Pescetti <andrea@nuvole.org>
2016-09-25 01:26:26 +0200

    Commands to create a basic project.

--- composer/commands.sh
+++ composer/commands.sh
@@ -6,3 +6,10 @@ php composer-setup.php
 
 # Move to system-wide location.
 sudo mv composer.phar /usr/local/bin/composer
+
+# Create empty directory.
+mkdir my-project
+cd my-project
+
+# Create new project by requiring a package.
+composer require twig/twig
