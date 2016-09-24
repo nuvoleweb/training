36ae71532784d9d68c9ba11c16ef1f9f1de89229
Andrea Pescetti <andrea@nuvole.org>
2016-09-25 01:54:17 +0200

    Fix typo.

--- composer/commands.sh
+++ composer/commands.sh
@@ -7,7 +7,7 @@ composer create-project drupal-composer/drupal-project:8.x-dev my-project --stab
 composer require drupal/ds 8.2.5
 
 # Add the admin_toolbar module as development dependency, version 8.x-1.10 to 8.x-2.0 not included.
-composer require-dev drupal/admin_toolbar ~8.1.10
+composer require --dev drupal/admin_toolbar ~8.1.10
 
 # Rebuild the codebase.
 composer update
