201ea02bdf906aa9299a0365b4b96e5a50d88c96
Andrea Pescetti <andrea@nuvole.org>
2016-09-25 01:23:18 +0200

    Commands to download Composer.

---
+++ composer/commands.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install composer as per https://getcomposer.org/download/
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php
+
+# Move to system-wide location
+sudo mv composer.phar /usr/local/bin/composer
