494d244f0150f5aa4380a3898d93bec1f229b6f8
Andrea Pescetti <andrea@nuvole.org>
2016-09-25 01:26:05 +0200

    Fix typo.

--- composer/commands.sh
+++ composer/commands.sh
@@ -4,5 +4,5 @@
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php composer-setup.php
 
-# Move to system-wide location
+# Move to system-wide location.
 sudo mv composer.phar /usr/local/bin/composer
