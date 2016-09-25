6c94e711ab3bdcc74162b8e0e74e04c7acfc2657
Andrea Pescetti <andrea@nuvole.org>
2016-09-25 02:03:24 +0200

    Change directory after building.

--- composer/commands.sh
+++ composer/commands.sh
@@ -2,6 +2,7 @@
 
 # Create a new Drupal project.
 composer create-project drupal-composer/drupal-project:8.x-dev my-project --stability dev --no-interaction
+cd my-project
 
 # Add the ds module, version 8.x-2.5.
 composer require drupal/ds 8.2.5
